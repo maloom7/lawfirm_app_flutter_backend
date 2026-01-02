4d1528d691e3e1624db3fe4252ce1a239a9d1458
maloom7 <118981316+maloom7@users.noreply.github.com>
2026-01-03 00:16:55 +0300

    update login and auth

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/test', function () {
-    return response()->json(['message' => 'API file is working ✅']);
-});
-
 Route::prefix('v1')->group(function () {
     Route::post('/auth/register', [AuthController::class, 'register']);
-    Route::post('/auth/login', [AuthController::class, 'login']);
+    Route::post('/auth/login',    [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/auth/me', [AuthController::class, 'me']);
-        Route::post('/auth/logout', [AuthController::class, 'logout']);
+        Route::get('/auth/me',     [AuthController::class, 'me']);
+        Route::post('/auth/logout',[AuthController::class, 'logout']);
     });
 });
